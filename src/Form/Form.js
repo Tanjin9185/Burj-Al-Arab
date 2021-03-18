263c15448c340346a7f752afdc80443023cfc48b
Tanjin <tanjin.ahamed123@gmail.com>
2021-03-18 16:18:40 +0600

    first commit

---
+++ src/Form/Form.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import './Form.css'
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const Form = () => {
+    const { register, handleSubmit, watch, errors } = useForm();
+    const [newUser, setNewUser] = useState(false);
+
+    const [user, setUser] = useState({
+        name: '',
+        email: '',
+        password: '',
+        error: '',
+        message: false,
+    })
+
+    const onSubmit = data => {
+        //  const newUserInfo = {...user};
+        setUser(data);
+
+        if (newUser && user.email && user.password) {
+            firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
+                .then((userCredential) => {
+                    // Signed in 
+                    var user = userCredential.user;
+                    console.log(user);
+                    const newUser = { ...user };
+                    newUser.message = true;
+                    newUser.error = '';
+                    setUser(newUser)
+                    // ...
+                })
+                .catch((error) => {
+                    // var errorCode = error.code;
+                    // var errorMessage = error.message;
+                    // console.log(errorCode,errorMessage);
+
+                    const newUser = { ...user };
+                    newUser.message = false;
+                    newUser.error = error.message;
+                    setUser(newUser)
+                    // ..
+                });
+        }
+
+        if (!newUser && user.email && user.password) {
+
+            firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+                .then((userCredential) => {
+                    // Signed in
+                    var user = userCredential.user;
+                    console.log(user);
+
+                    const newUser = { ...user };
+                    newUser.message = true;
+                    newUser.error = '';
+                    setUser(newUser)
+                    // ...
+                })
+                .catch((error) => {
+                    var errorCode = error.code;
+                    var errorMessage = error.message;
+                    const newUser = { ...user };
+                    newUser.message = false;
+                    newUser.error = errorMessage;
+                    setUser(newUser)
+                });
+        }
+    };
+
+    console.log(watch("example")); // watch input value by passing the name of it
+
+    return (
+        <div>
+            <input onClick={() => setNewUser(!newUser)} type="checkbox" name="newUser" id="" />
+            <label htmlFor="newUser">Register</label>
+
+            <form className="formStyle" onSubmit={handleSubmit(onSubmit)}>
+
+                {newUser && <input name="name" ref={register({ required: true, })} placeholder="Name" />} <br />
+                {errors.name && <span className="error">Name field is required</span>}<br />
+
+                <input name="email" ref={register({ required: true, pattern: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ })} placeholder="exampl.1@gmail.com" /><br />
+                {errors.email && <span className="error">Email field is required</span>}<br />
+
+                <input type="password" name="password" ref={register({ required: true, pattern: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/ })} placeholder="Password" /><br />
+                {errors.password && <span style={{ color: 'red' }}>Uppercase,Lowercase,Number,Special-character is required</span>}<br />
+
+                <input type="submit" value={newUser ? 'Sign-up' : 'Sign-in'} />
+            </form>
+
+            <p>{user.name}</p>
+            <p>{user.error}</p>
+
+            {
+                user.message && <p> User {newUser ? 'created' : 'logged in'} successfully </p>
+            }
+
+
+        </div>
+
+    );
+}
+
+export default Form;
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA0KU2I-QCgxQYyPKKKutgLWkGBuRge4W0",
+    authDomain: "burj-al-arab-38291.firebaseapp.com",
+    projectId: "burj-al-arab-38291",
+    storageBucket: "burj-al-arab-38291.appspot.com",
+    messagingSenderId: "252597076845",
+    appId: "1:252597076845:web:c09f13696358c2abf05064"
+  };
+
+  export default firebaseConfig;
